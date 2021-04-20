170351e21f2f8e19b2d2692d09d71aed873ddf6e
Nitish Kumar <nitishkmr096@gmail.com>
2021-04-20 15:15:06 +0530

    Initial Commit - React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  // below is JSX - Javascript + HTML
+
+  return (
+    <>
+      <h1>Welcome to Vulph-Cart</h1>
+    </>
+  );
+}
+
+export default App;
